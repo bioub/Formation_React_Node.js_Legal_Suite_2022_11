9104e8d0d6e97d674e5d100249ef74d7efaff797
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-11-08 11:21:08 +0100

    Correction Build

--- Exercice-Build/build.js
+++ Exercice-Build/build.js
@@ -1,14 +1,67 @@
-const fs = require('fs-extra');
-const path = require('path');
-const del = require('del');
-const md5 = require('md5');
-const UglifyJS = require('uglify-es');
+const fs = require("fs/promises");
+const path = require("path");
+const { minify } = require("terser");
+const { createHash } = require("crypto");
+const yargs = require('yargs/yargs');
+const argv = yargs(process.argv.slice(2)).argv
 
-const distPath = path.resolve(__dirname, 'dist');
-const srcPath = path.resolve(__dirname, 'src');
-const horlogeJsPath = path.resolve(srcPath, 'js', 'horloge.js');
-const indexJsPath = path.resolve(srcPath, 'js', 'index.js');
-const indexHtmlPath = path.resolve(srcPath, 'index.html');
-const indexHtmlDistPath = path.resolve(distPath, 'index.html');
-const appJsDistPath = path.resolve(distPath, 'app.js');
+const distPath = path.resolve(__dirname, "dist");
+const srcPath = path.resolve(__dirname, "src");
+const horlogeJsPath = path.resolve(srcPath, "js", "horloge.js");
+const indexJsPath = path.resolve(srcPath, "js", "index.js");
+const indexHtmlPath = path.resolve(srcPath, "index.html");
+const indexHtmlDistPath = path.resolve(distPath, "index.html");
+const appJsDistPath = path.resolve(distPath, "app.js");
 
+async function rmAndMkdir() {
+  await fs.rm(distPath, { recursive: true, force: true });
+  await fs.mkdir(distPath);
+  console.log('rm and mkdir dist OK');
+}
+
+async function buildJs() {
+  const bufferHorloge = await fs.readFile(horlogeJsPath);
+  const bufferIndex = await fs.readFile(indexJsPath);
+  let content = Buffer.concat([bufferHorloge, bufferIndex]);
+  let hash = '';
+
+  if (argv.minify) {
+    const { code } = await minify(content.toString());
+    content = code;
+  }
+
+  if (argv.hash) {
+    hash = createHash('md5').update(content).digest('hex');
+  }
+
+  const filename = hash ? appJsDistPath.replace('.js', `.${hash}.js`) : appJsDistPath;
+  await fs.writeFile(filename, content);
+  // await fs.writeFile(appJsDistPath, bufferHorloge);
+  // await fs.appendFile(appJsDistPath, bufferIndex);
+  console.log('build JS OK');
+  return hash;
+}
+
+async function buildHtml(hash) {
+  const buffer = await fs.readFile(indexHtmlPath);
+
+  const content = buffer
+    .toString('utf-8')
+    .replace(
+      '<script src="./js/horloge.js"></script>',
+      hash ? `<script src="./app.${hash}.js"></script>` : '<script src="./app.js"></script>'
+    )
+    .replace('<script src="./js/index.js"></script>', "");
+
+  await fs.writeFile(indexHtmlDistPath, content);
+  console.log('build HTML OK');
+}
+
+async function main() {
+  await rmAndMkdir();
+  const hash = await buildJs();
+  await buildHtml(hash);
+  console.log('build DONE');
+}
+
+main();
