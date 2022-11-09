b16f374283037dd15e17d4ebc99ba1b21d837436
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-11-09 16:15:39 +0100

    Fin Node.js

---
+++ Express/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const mongoose = require('mongoose');
+
+const config = require('./config');
+const app  = require('./app');
+
+const server = http.createServer(app);
+
+server.on('error', (err) => {
+  console.log(err.message);
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => {
+  server.listen(config.port, () => {
+    console.log('Server started on port ' + config.port);
+  });
+});
